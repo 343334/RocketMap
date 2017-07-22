f299a890d1a1dde14c0589e8a151281a1d369c92
Sébastien Vercammen <hello@sebastienvercammen.be>
2017-07-22 17:41:58 +0200

    Move setting to Store.

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -973,6 +973,10 @@ var StoreOptions = {
     'zoomLevel': {
         default: 16,
         type: StoreTypes.Number
+    },
+    'maxClusterZoomLevel': {
+        default: 14,
+        type: StoreTypes.Number
     }
 }
 
